c6032110e833b1416b74c53219bc5c0f96836aa7
shinichirot <78186249+shinichirot@users.noreply.github.com>
2021-01-29 12:54:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ var key = params.get('key');
 $(function () {
     // 送信
     $('form').submit(function () {
-        var reasons = $('textarea[name="name"]').val();
+        var name = $('textarea[name="name"]').val();
         // var date = $('input[name="date"]').val();
         // var tool = $('input[name="tool"]').val();
         
         //var msg = `${key}\n朝礼/終礼不参加の理由：${reasons}\n返信期限：${date}\n使用ツール：${tool}`;
-        var msg = `${key}\n名前：${name}`;
+        
+        var msg = `ご回答内容\n名前：${name}`;
         sendText(msg);
 
         return false;

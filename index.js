3c08a4d09466fdc963cb7905689b3a5764ddd120
shinichirot <78186249+shinichirot@users.noreply.github.com>
2021-01-29 14:54:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ var key = params.get('key');
 $(function () {
     // 騾∽ｿ｡
     $('form').submit(function () {
-        var reasons = $('textarea[name="reasons"]').val();
+        var name = $('textarea[name="name"]').val();
+        var adress = $('textarea[name="adress"]').val();
+        var date = $('textarea[name="date"]').val();
         // var date = $('input[name="date"]').val();
         // var tool = $('input[name="tool"]').val();
         
-        //var msg = `${key}\n譛晉､ｼ/邨ら､ｼ荳榊盾蜉縺ｮ逅�罰�${reasons}\n霑比ｿ｡譛滄剞�${date}\n菴ｿ逕ｨ繝��繝ｫ�${tool}`;
+        //var msg = `${key}\n名前${name}\n郵便番号${adress}\n面談希望日${date}`;
         var msg = `${key}\n逅�罰�${reasons}`;
         sendText(msg);
 

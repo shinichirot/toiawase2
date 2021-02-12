4740a33c73d498abdf9dc131eb3c648d7a80b4ee
shinichirot <78186249+shinichirot@users.noreply.github.com>
2021-02-12 10:37:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ var key = params.get('key');
 $(function () {
     // 騾∽ｿ｡
     $('form').submit(function () {
+
+        sendText(`◆回答内容`);
+        
         var name = $('textarea[name="name"]').val();
         var adress = $('textarea[name="adress"]').val();
         var tel = $('textarea[name="tel"]').val();
-        var shikaku = $('textarea[name="shikaku"]').val();
+        var shikaku = $('input[name="shikaku"]').val();
         var date = $('textarea[name="date"]').val();
         // var date = $('input[name="date"]').val();
         // var tool = $('input[name="tool"]').val();
        
-        sendText(`◆回答内容`);
         var msg = `職種：${key}\n名前：${name}\n郵便番号：${adress}\n電話番号：${tel}\資格：${shikaku}\n面談希望：${date}\n\n`;
         sendText(msg);
 

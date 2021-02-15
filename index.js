2edee893d919b75251095a832a932b665dbf01f5
shinichirot <78186249+shinichirot@users.noreply.github.com>
2021-02-15 15:15:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -11,13 +11,11 @@ $(function () {
         var adress = $('textarea[name="adress"]').val();
         var tel = $('textarea[name="tel"]').val();
         //var shikaku = $('input[name="shikaku"]').val();
-        var shikaku = request.getParameter("shikaku");
+        //var shikaku = request.getParameter("shikaku");
         
-        var date = $('input[name="date"]').val();
-        // var date = $('input[name="date"]').val();
-        // var tool = $('input[name="tool"]').val();
+        //var date = $('input[name="date"]').val();
        
-        var msg = `◆回答内容\n職種：${key}\n名前：${name}\n郵便番号：${adress}\n電話番号：${tel}\n資格：${shikaku}\n面談希望：${date}`;
+        //var msg = `◆回答内容\n職種：${key}\n名前：${name}\n郵便番号：${adress}\n電話番号：${tel}\n資格：${shikaku}\n面談希望：${date}`;
         sendText(msg);
 
         return false;

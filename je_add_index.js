39f33534e25fc9bfb3f9a02fba78dfe527223100
shinichirot <78186249+shinichirot@users.noreply.github.com>
2021-03-18 19:09:38 +0900

    Update je_add_index.js

--- je_add_index.js
+++ je_add_index.js
@@ -3,14 +3,7 @@ $(function () {
 
         sendText(`追加登録ありがとうございました`);
 
-        <!-- var move = $('input[name="move"]').val();
-        var days = $('select[name="days"]').val();
-        var in_date = $('input[name="in_date"]').val();
-        var other = $('textarea[name="other"]').val();
-              
-        var msg = `◆回答内容\n移動手段：${move}\n希望日数：${days}\n入社可能日：${in_date}\その他：${other}`;
-        sendText(msg);-->
-
+       
         return false;
     });
 });

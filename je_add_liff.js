fe748fc6de96aeb1be08111daf7ed7976e48444f
shinichirot <78186249+shinichirot@users.noreply.github.com>
2021-03-18 20:47:32 +0900

    Create je_add_liff.js

---
+++ je_add_liff.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    var liffId = "1655620211-lVL9X05e";
+    initializeLiff(liffId);
+})
+
+function initializeLiff(liffId) {
+    liff
+        .init({
+            liffId: liffId
+        })
+        .then(() => {
+          // start to use LIFF's api
+          initializeApp();
+        })
+        .catch((err) => {
+            console.log('LIFF Initialization failed ', err);
+        });
+}
+
+function sendText(text) {
+    // sendMessages(text);
+// }
+
+// LINEトーク画面上でメッセージ送信
+// function sendMessages(text) {
+    liff.sendMessages([{
+        'type': 'text',
+        'text': text
+    }]).then(function () {
+        liff.closeWindow();
+    }).catch(function (error) {
+        window.alert('Failed to send message ' + error);
+    });
+}
